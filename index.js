d9fe74b4140a6f7f1de5d730bb406a55eb2b1b58
andrewadriann <fernandodriel@gmail.com>
2021-06-20 20:09:26 +0700

    tahap3

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
-var express = require('express')
-var app = express()
-var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database("todo.db");
-db.run("CREATE TABLE IF NOT EXISTS list (deskripsi TEXT)");
+const express =  require('express');
+const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database("todos.db");
 
-const cors = require('cors')
-app.use(cors())
-
- const bpar = require('body-parser')
- app.use(bpar.urlencoded({extended:true}))
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended:true }))
+app.use(express.json())
 
 
- app.get('/', function (req, res) {
- 	res.send(`<html>
- 	<body>
- 	<form action="/todo" method="post">
- 	<input name="deskripsi"/>
- 	<button>Add</button>
- 	</form>
- </body>
- </html>
- `)
- })
-
- app.post('/todo', function (req, res) {
- 	res.send(
- 		db.serialize(function() {
-         var desc = req.body.deskripsi
-         var input = db.prepare("INSERT INTO list VALUES (?)")
-         input.run(desc) }))
- 	res.end()
- 	})
-
-app.get('/todo', function (req, res) {
- 	
-		db.serialize(function() {
- 			db.all("SELECT rowid AS id, deskripsi from list", function (err, row) {
-        		console.log(row)
-        		res.json(row)
-				res.end()
-			})
-		})
+app.get('/', (req,res) => {
+    res.send(`
+    <html>
+        <body>
+            <form action="/todo" method="post">
+            <input name="description" />
+            <button> Add </button>
+            </form>
+        </body>
+    </html>`)
 })
 
-app.post('/todo/:id',function(req,res) {
-	 var id=req.params.id;
-     var del = db.run("DELETE FROM list WHERE id=?")
-     del.run(id)
-     res.end()
-
+app.get('/todo', (req,res) => {
+    var sql ='SELECT * from `todo`'
+    db.all(sql, (err,data) => {
+        if (err) throw err
+        res.send(data);
+    })
 })
 
- app.listen(3000)
+app.post('/todo', (req, res) => {
+     let sql = `INSERT INTO todo(description) VALUES (?)`
+     let values = [req.body.description]
+     db.run(sql, values, function(err) {
+        if (err){
+      res.status(500).json({'error': err.message});
+      return;
+    }
+    res.status(200).json('Insert Berhasil');
+      })
+})
+
+app.delete('/todo/:id', function(req,res){
+db.run('DELETE FROM `todo` WHERE id = ?',req.params.id,function (err, result) {
+        if (err){
+            res.status(400).json({'error': res.message})
+            return;
+        }
+        res.json({'message': 'Delete berhasil.'})
+});
+})
+
+app.listen(3000, function() {
+  console.log('Server listens on port 3000');
+});
